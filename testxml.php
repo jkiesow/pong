3a175a44f9f8c29304a9e5fc5209c20ff8a8c497
florianklaessen <florian.klaessen@smail.fh-koeln.de>
2015-11-18 14:42:41 +0100

    testxml ''

--- testxml.php
+++ testxml.php
@@ -1,25 +1,25 @@
  <?php
      $xml = simplexml_load_file ( 'bla.xml' );
 
-     $pong = $xml->xpath("pong");
+     $pong = $xml->xpath('pong');
 
-     $player = $pong->addchild("player");
+     $player = $pong->addchild('player');
 
-     $game = $pong->addchild("game");
+     $game = $pong->addchild('game');
      
    //  $pong->addAttribute('',''); 
 
-     $player->addChild("name","test");
+     $player->addChild('name','test');
 
-     $player->addChild("win","1");
+     $player->addChild('win','1');
 
-     $game->addChild("namep1",$_GET['namep1']);
+     $game->addChild('namep1',$_GET['namep1']);
 
-     $game->addChild("namep2",$_GET['namep2']);
+     $game->addChild('namep2',$_GET['namep2']);
     
-     $game->addChild("scorep1",$_GET['scorep1']);
+     $game->addChild('scorep1',$_GET['scorep1']);
 
-     $game->addChild("scorep2",$_GET['scorep2']);
+     $game->addChild('scorep2',$_GET['scorep2']);
 
      echo $xml->asXML( 'bla.xml' );
  ?>
