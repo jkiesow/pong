6b926898e00dd1383e0167d86ae9dca105dd24f2
florianklaessen <florian.klaessen@smail.fh-koeln.de>
2015-11-24 20:35:42 +0100

    update
    bla

--- read.php
+++ read.php
@@ -1,12 +1,15 @@
 <?php
-    $xml = simplexml_load_file ( 'bla.xml' );
-    
-    foreach($xml->game as $child){
-        if($child->scorep1 >= 9 || $child->scorep2 >= 9){
-            $child->running="false";
-            $dom=dom_import_simplexml($child);
-            $dom->parentNode->removeChild($dom);
-        }
+    $dom = new DOMDocument();
+    $dom->load( 'bla.xml' );
+    $domElemsToRemove = array();
+    $games=$dom->getElementsByTagName( "game" );
+    foreach( $games as $child){
+        if($child->getElementsByTagName( "scorep1" )->nodevalue == 9 || $child->getElementsByTagName("scorep2")->nodevalue == 9){
+            echo "ping";
+            $child->getElementsByTagName("running")->nodevalue = "false";
+            $domElemsToRemove[] = $child;
+            
+            }
         if($child->running=="true"){
             echo "<table>";
             echo "<tr>";
@@ -20,10 +23,13 @@
             echo "<td>", $child->scorep1, " : ", $child->scorep2, "</td>";
             echo "</tr>";
             echo "</table>";
-
+            
         }
         
     
     }    
-
+        foreach( $domElemsToRemove as $child ){
+        $child->parentNode->removeChild($child);
+        }
+echo $dom->save( 'bla.xml' );
 ?>
